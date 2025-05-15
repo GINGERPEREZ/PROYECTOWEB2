2cfe1546f09d68bdc79d0699ae223b9de9e9493c
GINGERPEREZ <gingerperez160@gmail.com>
2025-05-15 16:33:00 -0500

    Eliminar todo el contenido de master

--- practica2/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import { AppDataSource } from "./config/data-source";
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Conexión a la base de datos exitosa.");
-  })
-  .catch((error) => console.error("Error en la conexión:", error));
